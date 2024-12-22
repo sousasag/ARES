8b3e0b47c40525771a051a388b8fbe84300d670c
Sergio Sousa <sousasag@gmail.com>
2024-12-22 14:54:09 +0000

    updating more tests

--- ares_cython/tests/testing_arespy.py
+++ ares_cython/tests/testing_arespy.py
@@ -26,5 +26,32 @@ class TestARESpy(unittest.TestCase):
         rejt = ares.get_rejtpy("3;5764,5766,6047,6053,6068,6076", llcor, flux)
         self.assertAlmostEqual(rejt, 0.997048,6, "Should be 0.997048")
 
+
+    def test_getpixelline(self):
+        line_test = 6005.01
+        ll = np.linspace(6000,6100,10001)
+        index = ares.find_pixel_linepy(ll, line_test)
+        self.assertEqual(line_test,ll[index],"Should be "+str(line_test))
+
+    def test_continuum_run(self):
+        spectrum='../../sun_harps_ganymede.fits'
+        ll, flux = ares.read_spec(spectrum)
+        locind = np.where((ll > 6002 ) & (ll <6008))
+        x=ll[locind]
+        y=flux[locind]
+        ynorm,res = ares.continuum_det5py(x,y,0.99)
+        ch = int(len(y)/4)
+        meanloc = np.mean(np.sort(ynorm)[-ch:])
+        print("Mean local normalized continuum: ", meanloc)
+        self.assertAlmostEqual(meanloc,1,1,"Should be close to 1")
+
+
+#    def test_getlines(self):
+#        spectrum='../../sun_harps_ganymede.fits'
+#        ll, flux = ares.read_spec(spectrum)
+#        ll = ares.correct_lambda_rvpy(ll.copy(), flux, "0,0.1")
+#        lc, ld, i1, i2, x, ynorm = getMedida_lines(ll, flux, line, space, rejt, distline)
+        
+
 if __name__ == '__main__':
     unittest.main()
